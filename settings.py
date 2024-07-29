fb8031458c30fee8d5ce3ca973e0663fa0d7e867
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-07-29 23:05:15 +0300

    add info to settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+EMPLOYERS_IDS = [
+        "https://api.hh.ru/employers/78638",
+        "https://api.hh.ru/employers/3529",
+        "https://api.hh.ru/employers/2180",
+        "https://api.hh.ru/employers/1740",
+        "https://api.hh.ru/employers/89",
+        "https://api.hh.ru/employers/999604",
+        "https://api.hh.ru/employers/783222",
+        "https://api.hh.ru/employers/5557093",
+        "https://api.hh.ru/employers/1141909",
+        "https://api.hh.ru/employers/2365329"
+    ]
